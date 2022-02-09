98b83eb68213a7ac3f95417d52cef75e7c2a12c2
Liran <Liranpro54@gmail.com>
2022-02-09 17:56:54 +0200

    adding test, will move to test dir later

---
+++ test.py
@@ -0,0 +1,16 @@
+import fs.copy
+from fs.memoryfs import MemoryFS
+from fs.osfs import OSFS
+from songs import songs_handler
+
+
+def main():
+    # mem_fs = MemoryFS()
+    # drv_fs = OSFS("~liran/Documents/songs")
+    songs_handler.merge_songs_files()
+    # fs.copy.copy_fs(drv_fs, mem_fs)
+    # print(mem_fs.listdir('.'))
+
+
+if __name__ == '__main__':
+    main()

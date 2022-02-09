b61fae02408f1de3ffd277f2a8bb13c883dafaff
Liran <Liranpro54@gmail.com>
2022-02-09 20:58:11 +0200

    new playlist creation and adding a song to playlist methods

--- users/user.py
+++ users/user.py
@@ -9,5 +9,22 @@ class User:
         self.password = str
         self.playlist = {str: list}
 
-    def add_song_to_playlist(self):
-        pass
+    def add_song_to_playlist(self, song_name, playlist_name):
+        if self.is_premium:
+            self.playlist[playlist_name].append(song_name)
+            print("added new song!")
+        else:
+            if len(self.playlist[playlist_name]) != 20:
+                self.playlist[playlist_name] = []
+            else:
+                print("no can do, pay up!")
+
+    def create_playlist(self, playlist_name):
+        if self.is_premium:
+            self.playlist[playlist_name] = []
+            print("created a new playlist")
+        else:
+            if len(self.playlist) != 5:
+                self.playlist[playlist_name] = []
+            else:
+                print("no can do, pay up!")

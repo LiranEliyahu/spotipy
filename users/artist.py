2a152806c1bb47117ccfe73b470a45090f09fcca
Liran <Liranpro54@gmail.com>
2022-02-10 10:01:48 +0200

    added funcs and flow changes to the class

--- users/artist.py
+++ users/artist.py
@@ -5,3 +5,6 @@ class Artist(User):
     def __init__(self, user_name, password):
         super().__init__(user_name, password)
         self.is_premium = True
+
+    def get_artist_id(self):
+        pass

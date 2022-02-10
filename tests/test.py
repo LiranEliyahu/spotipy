be6f45ae531335641cc3201c706aae70f742ac78
Liran <Liranpro54@gmail.com>
2022-02-10 08:01:11 +0200

    changed the names of the dirs

--- tests/test.py
+++ tests/test.py
@@ -1,11 +1,12 @@
 import fs.copy
 from fs.memoryfs import MemoryFS
 from fs.osfs import OSFS
+from users.artist import Artist
 from songs import songs_handler
 
 
 def main():
-    pass
+    songs_handler.merge_songs_files()
     # mem_fs = MemoryFS()
     # drv_fs = OSFS("~liran/Documents/songs")
     # fs.copy.copy_fs(drv_fs, mem_fs)

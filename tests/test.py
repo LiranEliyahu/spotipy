1d2583e42fb9d01a8aeaf2f5a4424b0d36b84294
Liran <Liranpro54@gmail.com>
2022-02-10 10:02:20 +0200

    added some testing code for moudels

--- tests/test.py
+++ tests/test.py
@@ -1,16 +1,16 @@
-import fs.copy
-from fs.memoryfs import MemoryFS
-from fs.osfs import OSFS
-from users.artist import Artist
 from songs import songs_handler
+from search import search
 
 
 def main():
-    songs_handler.merge_songs_files()
-    # mem_fs = MemoryFS()
-    # drv_fs = OSFS("~liran/Documents/songs")
-    # fs.copy.copy_fs(drv_fs, mem_fs)
-    # print(mem_fs.listdir('.'))
+    songs = songs_handler.merge_songs_files()
+
+    print(songs)
+    print(search.find_all_artists(songs))
+    print(search.get_artist_songs(songs, "Margalit Tzan'ani"))
+    print(search.get_top_ten(songs, "Margalit Tzan'ani"))
+    print(search.get_album_songs("Bringing It All Back Home", songs))
+    print(search.get_artist_album(songs, "Margalit Tzan'ani"))
 
 
 if __name__ == '__main__':

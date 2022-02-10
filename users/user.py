3451216daea87999bb7b08dfcf152551bef728c1
Liran <Liranpro54@gmail.com>
2022-02-10 10:01:19 +0200

    added more methods to the user class

--- users/user.py
+++ users/user.py
@@ -1,30 +1,34 @@
 from uuid import uuid1
+from passlib.context import CryptContext
 
 
 class User:
-    def __init__(self, user_name, password):
-        self.user_id = str(uuid1())
-        self.is_premium = bool
-        self.user_name = str
-        self.password = str
+    def __init__(self, user_name, password, premium=False):
+        self.id = str(uuid1())
+        self.is_premium = premium
+        self.user_name = user_name
+        self.password = password
         self.playlist = {str: list}
 
     def add_song_to_playlist(self, song_name, playlist_name):
         if self.is_premium:
             self.playlist[playlist_name].append(song_name)
-            print("added new song!")
+            return True
         else:
             if len(self.playlist[playlist_name]) != 20:
                 self.playlist[playlist_name] = []
             else:
-                print("no can do, pay up!")
+                return False
 
     def create_playlist(self, playlist_name):
         if self.is_premium:
             self.playlist[playlist_name] = []
-            print("created a new playlist")
+            return True
         else:
             if len(self.playlist) != 5:
                 self.playlist[playlist_name] = []
             else:
-                print("no can do, pay up!")
+                return False
+
+    def verify_user(self, password, user_name, context):
+        return True if user_name == self.user_name and context.verify(password) else False
